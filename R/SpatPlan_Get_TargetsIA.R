e28766735029b3e53e953eea6aa41eda884bb835
Jason Everett <jaseeverett@gmail.com>
2022-04-11 16:47:10 +1000

    Minor updates  - .data$

--- R/SpatPlan_Get_TargetsIA.R
+++ R/SpatPlan_Get_TargetsIA.R
@@ -19,9 +19,9 @@ SpatPlan_Get_TargetsIA <- function(df, target_min, target_max){
     dplyr::select(-.data$geometry) %>%
     dplyr::summarise(dplyr::across(dplyr::everything(), ~ sum(., is.na(.), 0))) %>%
     tidyr::pivot_longer(dplyr::everything(.), names_to = "Species", values_to = "Area_km2") %>%
-    dplyr::mutate(Species = stringr::str_replace_all(Species, pattern = "_", replacement = " "),
-           Area_km2 = Area_km2 * PU_area_km2,
-           Target = target_max-((Area_km2/total_PU_area)*(target_max-target_min)))
+    dplyr::mutate(Species = stringr::str_replace_all(.data$Species, pattern = "_", replacement = " "),
+           Area_km2 = .data$Area_km2 * PU_area_km2,
+           Target = target_max-((.data$Area_km2/total_PU_area)*(target_max-target_min)))
 
 }
 
