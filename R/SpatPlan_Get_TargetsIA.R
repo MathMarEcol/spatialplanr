557c8926fe117823bd62a4d9bc670cd00d31c24d
Jason Everett <jaseeverett@gmail.com>
2022-05-24 19:43:02 +1000

    Fix .date

--- R/SpatPlan_Get_TargetsIA.R
+++ R/SpatPlan_Get_TargetsIA.R
@@ -21,7 +21,7 @@ SpatPlan_Get_TargetsIA <- function(df, target_min, target_max){
     dplyr::as_tibble() %>%
     dplyr::select(-.data$geometry) %>%
     dplyr::summarise(dplyr::across(dplyr::everything(), ~ sum(.data, is.na(.data), 0))) %>%
-    tidyr::pivot_longer(dplyr::everything(.), names_to = "Species", values_to = "Area_km2") %>%
+    tidyr::pivot_longer(dplyr::everything(.data), names_to = "Species", values_to = "Area_km2") %>%
     dplyr::mutate(Species = stringr::str_replace_all(.data$Species, pattern = "_", replacement = " "),
            Area_km2 = .data$Area_km2 * PU_area_km2,
            Target = target_max-((.data$Area_km2/total_PU_area)*(target_max-target_min)))
