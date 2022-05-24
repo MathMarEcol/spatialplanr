d23e9de6a342cc4c0739465dc2ccbfcf8d7ddbd5
Jason Everett <jaseeverett@gmail.com>
2022-05-24 16:46:09 +1000

    Clean up code

--- R/SpatPlan_Get_Geomorphic.R
+++ R/SpatPlan_Get_Geomorphic.R
@@ -1,7 +1,3 @@
-
-
-
-
 #' Assign geomorphic features to each planning unit.
 #'
 #' Options for `Feature` are:
@@ -17,38 +13,40 @@
 #' Geomorphology of the oceans. Marine Geology, 352: 4-24.
 #' Licensed under a Creative Commons Attribution 4.0 International License
 #'
-#' @param PUs Planning units sf object
+#' @param PlanUnits Planning Units as an `sf` object
 #' @param Feature A vector of the geomprphic features you wish to retain. Defaults to all.
 #' @param Direc The directory where the MME data is being stored. If not specified, the default location is assumed.
 #'
-#' @return
+#' @return An `sf` object of Geomorphic features by planning unit.
 #' @export
 #'
 #' @examples
-#' \dontrun{features <- SpatPlan_Get_Geomorphic(PUs, Feature = "Seamount")}
+#' \dontrun{features <- SpatPlan_Get_Geomorphic(PlanUnits, Feature = "Seamount")}
 #' @importFrom rlang .data
 #' @importFrom rlang :=
-SpatPlan_Get_Geomorphic <- function(PUs,
+SpatPlan_Get_Geomorphic <- function(PlanUnits,
                                     Feature = c("Abyss", "Abyss_Hills", "Abyss_Mountains", "Abyss_Plains", "Basin",
                                                 "Bridge", "Canyon", "Escarpment", "Fan", "Glacial Trough", "Guyot",
                                                 "Hadal", "Plateau", "Ridge", "Rift Valley", "Rise", "Seamount",
-                                                "Shelf", "Shelf Valley", "Shelf_medium", "Shelf_high", "Shelf_low",
+                                                "Shelf", "Shelf Valley", "Shelf_high", "Shelf_medium", "Shelf_low",
                                                 "Sill", "Slope", "Spreading Ridge", "Terrace", "Trench", "Trough"),
                                     Direc = file.path("~", "SpatPlan_Data")){
 
   dat <- readRDS(file.path(Direc, "GeomorphicData.rds")) %>%
-    dplyr::filter(.data$Geomorphic %in% Feature) %>%
     dplyr::select(.data$Geomorphic) %>%
-    sf::st_transform(sf::st_crs(PUs)) %>%
+    dplyr::filter(.data$Geomorphic %in% Feature) %>%
+    sf::st_transform(sf::st_crs(PlanUnits)) %>%
     sf::st_make_valid() %>%
-    sf::st_crop(PUs)
-
+    sf::st_crop(PlanUnits)
 
   for (f in 1:length(Feature)){
-    PUs <- SpatPlan_Convert_Poly2PUs(dat %>% dplyr::filter(.data$Geomorphic == Feature[f]) %>% dplyr::rename(!!Feature[f] := .data$Geomorphic),
-                                     PUs, binary = TRUE)
+
+    PlanUnits <- SpatPlan_Convert_2PUs(dat %>%
+                                   dplyr::filter(.data$Geomorphic == Feature[f]) %>%
+                                   dplyr::rename(!!Feature[f] := .data$Geomorphic),
+                                 PlanUnits)
   }
 
-  return(PUs)
+  return(PlanUnits)
 
 }
