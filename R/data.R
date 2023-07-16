340d49b69b30ae7ed83af4b246362b4e341d0d48
Jason Everett <jaseeverett@gmail.com>
2023-07-16 14:11:10 +1000

    Add data documentation

---
+++ R/data.R
@@ -0,0 +1,117 @@
+#' Planning Units
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_PUs`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_PUs"
+
+
+# -------------------------------------------------------------------------
+
+#' Boundary of planning units
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_bndry`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_bndry"
+
+# -------------------------------------------------------------------------
+
+
+#' Regionalisation
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_region`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_region"
+
+
+
+# -------------------------------------------------------------------------
+
+#' prioritizr Solution
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_soln`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_soln"
+
+
+
+# -------------------------------------------------------------------------
+
+#' prioritizr Solution2
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_soln2`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_soln2"
+
+
+# -------------------------------------------------------------------------
+
+#' Binary Species Data
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_species_bin`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_species_bin"
+
+
+# -------------------------------------------------------------------------
+
+#' Probability Species Data
+#'
+#' An sf dataframe of planning units for spatial prioritisation
+#'
+#' @format ## `dat_species_prob`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#'   \item{cellID}{ID number for each row (or cell) of the dataset}
+#'   \item{geometry}{sf geometry column}
+#'   ...
+#' }
+#' @source "Made up data"
+"dat_species_prob"
