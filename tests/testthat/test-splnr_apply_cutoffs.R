2e40d3165de03d22cff2f604013e40ed35dab748
Jason Everett <jaseeverett@gmail.com>
2023-11-11 14:27:41 +1100

    Update test-splnr_apply_cutoffs.R

--- tests/testthat/test-splnr_apply_cutoffs.R
+++ tests/testthat/test-splnr_apply_cutoffs.R
@@ -4,3 +4,18 @@ testthat::test_that("Correct function output", {
     splnr_apply_cutoffs(dat_species_prob, Cutoffs = 0.5), "sf"
   )
 })
+
+testthat::test_that("Correct function output", {
+  expect_s3_class(
+    splnr_apply_cutoffs(dat_species_prob, Cutoffs = 0.5, inverse = TRUE), "sf"
+  )
+})
+
+
+testthat::test_that("Correct function output", {
+  expect_s3_class(
+    splnr_apply_cutoffs(dat_species_prob,
+                        Cutoffs = c("Spp1" = 0.5, "Spp2" = 0.4, "Spp3" = 0.6, "Spp4" = 0.5, "Spp5" = 0.5)), "sf"
+  )
+})
+
