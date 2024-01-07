4b9944f1a00eb30271750998e8914eff83d6c51d
Kilian BARREIRO <kiliangrihaultbarreiro@gmail.com>
2024-01-08 09:38:48 +1000

    Update test-splnr_get_distCoast.R

--- tests/testthat/test-splnr_get_distCoast.R
+++ tests/testthat/test-splnr_get_distCoast.R
@@ -20,5 +20,9 @@ testthat::test_that("Correct function output", {
   expect_s3_class(result, "sf")
 })
 
-
+testthat::test_that("Correct function input", {
+  non_sf_object <- data.frame(x = 1:10, y = 11:20)
+  
+  expect_error(splnr_get_distCoast(non_sf_object), "Input data should be an 'sf' spatial object.")
+})
 
