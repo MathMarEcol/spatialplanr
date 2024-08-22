1810766ce58369800ef3f881d3651614cf8aa2e7
Jason Everett <jaseeverett@gmail.com>
2024-08-22 10:52:40 +1000

    Update logo and favicon

--- data-raw/CreateHex.R
+++ data-raw/CreateHex.R
@@ -17,33 +17,47 @@ Shape <- "Hexagon" # "Shape of PUs
 # cCRS <- "ESRI:54030" # Robinson
 cCRS <- "EPSG:4326"
 
-blx <- 5 # bottom left x
-brx <- 145 # bottom right x
 
-xi1 <- seq(75, brx, 1) # Top right
-yi1 <- pracma::interp1(c(75, brx), c(80, 40), xi1, method = "linear")
 
-yi2 <- seq(40, -30, -1) # Right side
-xi2 <- pracma::interp1(c(40, -30), c(brx, brx), yi2, method = "linear")
+create_hexagon <- function(center_x, center_y, size, top_type) {
+  if (top_type == "flat" || top_type == "flat_topped") {
+    angles <- seq(0, 300, by = 60)
+  } else if (top_type == "pointy" || top_type == "pointed" || top_type == "pointed_top") {
+    angles <- seq(30, 360, by = 60)
+  } else {
+    stop("Invalid top_type. Must be 'flat' or 'pointy'.")
+  }
 
-xi3 <- seq(brx, 75, -1) # Bottom right
-yi3 <- pracma::interp1(c(brx, 75), c(-30, -70), xi3, method = "linear")
+  vertices <- matrix(0, nrow = length(angles), ncol = 2)
 
-xi4 <- seq(75, blx, -1) # Bottom left
-yi4 <- pracma::interp1(c(75, blx), c(-70, -30), xi4, method = "linear")
+  for (i in 1:length(angles)) {
+    angle <- angles[i]
+    vertices[i, 1] <- center_x + size * cos(angle * pi / 180)
+    vertices[i, 2] <- center_y + size * sin(angle * pi / 180)
+  }
 
-yi5 <- seq(40, -30, -1) # Left side
-xi5 <- pracma::interp1(c(40, -30), c(blx, blx), yi5, method = "linear")
+  # Close the polygon by duplicating the first vertex
+  vertices <- rbind(vertices, vertices[1, ])
 
-xi6 <- seq(blx, 75) # Top left
-yi6 <- pracma::interp1(c(blx, 75), c(40, 80), xi6, method = "linear")
+  out <- list()
+  for (i in 1:length(angles)) {
+    int <- sf::st_linestring(vertices[i:(i+1),]) %>%
+      sf::st_segmentize(units::set_units(0.1, km)) %>%
+      sf::st_coordinates()
 
-Bndry <- dplyr::tibble(
-  x = c(xi1, xi2, xi3, xi4, xi5, xi6),
-  y = c(yi1, yi2, yi3, yi4, yi5, yi6)
-) %>%
-  as.matrix() %>%
-  list() %>%
+    out[[i]] <- int[,1:2]
+
+  }
+
+  out2 <- list(do.call(rbind, out))
+
+  # Create an sf polygon
+  polygon <- sf::st_polygon(out2)
+
+  return(polygon)
+}
+
+Bndry <- create_hexagon(70, -5, 70*1.1547, "pointed") %>%
   sf::st_polygon() %>%
   sf::st_sfc(crs = "EPSG:4326") %>%
   sf::st_sf() %>%
@@ -91,15 +105,16 @@ PUs <- PUs %>%
 
 hexSticker::sticker(gg,
   package = "spatialplanr",
-  p_y = 1,
   p_x = 1,
+  p_y = 0.98,
   p_color = "white",
-  p_size = 80,
+  p_family = "Aller_Rg",
   p_fontface = "bold",
+  p_size = 80,
   s_x = 1,
   s_y = 1,
-  s_width = 2,
-  s_height = 4,
+  s_width = 2.2,
+  s_height = 2.2,
   # h_fill = "#9FE2BF",
   h_color = "#2e1707", # "grey40",
   dpi = 1000,
@@ -108,32 +123,4 @@ hexSticker::sticker(gg,
 )
 
 # Create favicons for the site
-pkgdown::build_favicons(pkg = ".", overwrite = FALSE)
-
-
-# Original
-
-#
-# (gg <- ggplot2::ggplot() +
-#     ggplot2::geom_sf(data = PUs, colour = "black", linewidth = 0.1, show.legend = FALSE, ggplot2::aes(fill = Prob)) +
-#     ggplot2::geom_sf(data = landmass, colour = "grey30", fill = "grey50", alpha = 1, linewidth = 0.1, show.legend = FALSE) +
-#     ggplot2::coord_sf(xlim = sf::st_bbox(PUs)$xlim, ylim = sf::st_bbox(PUs)$ylim) +
-#     ggplot2::theme_void())
-#
-#
-# hexSticker::sticker(gg,
-#                     package = "spatialplanr",
-#                     p_y = 1,
-#                     p_x = 1,
-#                     p_color = "grey90",
-#                     p_size = 80,
-#                     p_fontface = "bold",
-#                     s_x = 1,
-#                     s_y = 1,
-#                     s_width = 2,
-#                     s_height = 4,
-#                     # h_fill = "#9FE2BF",
-#                     h_color = "grey20",
-#                     dpi = 1000,
-#                     asp = 1,
-#                     filename = file.path("man", "figures", "logo.png"))
+pkgdown::build_favicons(pkg = ".", overwrite = TRUE)
